da75faffa5bcc28e98c8484063038097f0aabf9f
ismorodin@hotmail.com <ismorodin@hotmail.com>
2014-03-28 11:51:40 +0400

    new template for langPage

--- tpl-visitors/js/main.js
+++ tpl-visitors/js/main.js
@@ -1,3 +1,29 @@
-/**
- * Created by Иван on 28.03.14.
- */
+$(document).ready(function () {
+	var root = $('#sortablemain');
+	$('> *', root).each(function (index) {
+		this.id = 'item-' + index;
+	});
+
+	root.sortable({
+		'update': function (event, ui) {
+			var order = $(this).sortable('serialize');
+			$.cookie('sortable', order);
+		}
+	});
+	var c = $.cookie('sortable');
+	if (c) {
+		$.each(c.split('&'), function () {
+			var id = this.replace('[]=', '-');
+			$('#' + id).appendTo(root);
+		});
+	}
+	root.sortable({
+		'update': function (event, ui) {
+			var order = $(this).sortable('serialize');
+			$.ajax({
+				url: '/sortable/save',
+				data: {'sortable': order, timestamp: $.now()},
+				type: 'post'
+			});
+		}});
+});
